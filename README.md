73156e33d13d114aab20fffb95dd4874dc853cef
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2021-03-16 15:00:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tox-strict
+Tox plugin which adds few extra checks like assuring that git does not report dirty
---
+++ src/tox_strict/hooks.py
@@ -0,0 +1,29 @@
+"""Tox hook implementations."""
+from __future__ import print_function
+
+import logging
+import os
+
+import git
+
+try:
+    from tox import hookimpl
+    from tox.reporter import error
+
+    @hookimpl
+    def tox_runtest_post(venv):
+        """Hook that runs after test commands."""
+        if os.path.isdir(f"{venv.envconfig.config.toxinidir}/.git"):
+            repo = git.Repo(".")
+            if repo.is_dirty():
+                error(
+                    "Git reported dirty status. "
+                    "Git should never report dirty status at the end of "
+                    "testing, regardless if status is passed, failed or aborted."
+                )
+                venv.status = "failed"
+
+
+except ImportError:
+    # tox4
+    logging.error("tox-strict disabled itself as it does not support tox4 yet.")
