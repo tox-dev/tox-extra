7136d3cb798b6b738c478fb463dc246ee0209ef8
Sorin Sbarnea <ssbarnea@redhat.com>
2021-04-21 10:14:15 +0100

    Improve git dirty checks (#7)

--- src/tox_extra/hooks.py
+++ src/tox_extra/hooks.py
@@ -14,14 +14,26 @@ try:
     def tox_runtest_post(venv):
         """Hook that runs after test commands."""
         if os.path.isdir(f"{venv.envconfig.config.toxinidir}/.git"):
-            repo = git.Repo(".")
-            if repo.is_dirty():
-                error(
-                    "Git reported dirty status. "
-                    "Git should never report dirty status at the end of "
-                    "testing, regardless if status is passed, failed or aborted."
-                )
-                venv.status = "failed"
+            _environ = dict(os.environ)
+            try:
+                # Force git to ignore global or user config as we do not want
+                # it to consider upper level ignores that may not exist on
+                # other systems.
+                os.environ["GIT_CONFIG_NOSYSTEM"] = "1"
+                os.environ["XDG_CONFIG_HOME"] = "/"
+                os.environ["HOME"] = "/"
+                repo = git.Repo(os.getcwd())
+                if repo.is_dirty(untracked_files=True):
+                    error(
+                        "Git reported dirty status. "
+                        "Git should never report dirty status at the end of "
+                        "testing, regardless if status is passed, failed or aborted."
+                    )
+                    os.system("git status")
+                    venv.status = "failed"
+            finally:
+                os.environ.clear()
+                os.environ.update(_environ)
 
 
 except ImportError:
