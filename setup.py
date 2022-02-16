410f4fb54cbd077204d94ce5968ee4e22bc0c632
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-16 11:00:59 +0000

    Remove setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-"""Setup"""
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup(
-        use_scm_version={"local_scheme": "no-local-version"},
-        setup_requires=["setuptools_scm[toml]>=3.5.0"],
-    )
