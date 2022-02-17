ac3c31086a4385a85455023527184b390df793cb
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-17 08:14:26 +0000

    Add support for tox 4 (#36)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/tox_extra/hooks.py
+++ src/tox_extra/hooks.py
@@ -1,34 +1,54 @@
 """Tox hook implementations."""
-from __future__ import print_function
-
-import logging
 import os
 
 import git
 
-try:
+MSG_GIT_DIRTY = (
+    "Git reported dirty status. "
+    "Git should never report dirty status at the end of "
+    "testing, regardless if status is passed, failed or aborted."
+)
+
+
+def is_git_dirty(path: str) -> bool:
+    """Reports if the git repository at the given path is dirty."""
+    if os.path.isdir(f"{path}/.git"):
+        _environ = dict(os.environ)
+        try:
+            repo = git.Repo(os.getcwd())
+            if repo.is_dirty(untracked_files=True):
+                os.system("git status")
+                return True
+        finally:
+            os.environ.clear()
+            os.environ.update(_environ)
+    return False
+
+
+try:  # tox3 support
     from tox import hookimpl
     from tox.reporter import error
 
     @hookimpl
     def tox_runtest_post(venv):
         """Hook that runs after test commands."""
-        if os.path.isdir(f"{venv.envconfig.config.toxinidir}/.git"):
-            _environ = dict(os.environ)
-            try:
-                repo = git.Repo(os.getcwd())
-                if repo.is_dirty(untracked_files=True):
-                    error(
-                        "Git reported dirty status. "
-                        "Git should never report dirty status at the end of "
-                        "testing, regardless if status is passed, failed or aborted."
-                    )
-                    os.system("git status")
-                    venv.status = "failed"
-            finally:
-                os.environ.clear()
-                os.environ.update(_environ)
+        if is_git_dirty(venv.envconfig.config.toxinidir):
+            venv.status = "failed"
+            error(MSG_GIT_DIRTY)
 
-except ImportError:
-    # tox4
-    logging.error("tox-extra disabled itself as it does not support tox4 yet.")
+except ImportError:  # tox4 support
+    from typing import List
+
+    from tox.execute import Outcome
+    from tox.plugin import impl
+    from tox.tox_env.api import ToxEnv
+    from tox.tox_env.errors import Fail
+
+    @impl
+    # pylint: disable=unused-argument
+    def tox_after_run_commands(
+        tox_env: ToxEnv, exit_code: int, outcomes: List[Outcome]
+    ) -> None:
+        """Hook that runs after test commands."""
+        if is_git_dirty("."):
+            raise Fail(MSG_GIT_DIRTY)
